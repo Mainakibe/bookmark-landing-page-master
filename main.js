04f187297d2458c57aafbebfbd1259c1a1e11e69
Maina Kibe <66884954+Mainakibe@users.noreply.github.com>
2021-05-27 20:40:41 +0300

    modified the js

--- main.js
+++ main.js
@@ -25,11 +25,11 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 // accordion
-const accordionQuestions=document.querySelectorAll(".accordion-question")
+const accordionQuestions=document.querySelectorAll(".accordion-question");
 
 accordionQuestions.forEach(accordionQuestion=>{
     accordionQuestion.addEventListener("click",event=>{
-        accordionQuestion.classList.toggle("active")
+        accordionQuestion.classList.toggle("active");
     })
 })
 
